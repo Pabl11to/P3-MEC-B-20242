a355a9daabacdb4a7fb9d001e2120a62f49da863
Pabl11to <ov.pablo8@gmail.com>
2024-08-27 14:35:10 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,15 @@
+/*Crear una clase llamada fracion con atributos correspondientes a numerador y denominador, aplicar encapsula, y los metodos set y get necesario*/
+ 
+public class proyecto{
+  public class fraccion {
+    public int num;
+    public int denm;
+    
+    public set. 
+    
+  
+    
+    
+}
+   
+ }
