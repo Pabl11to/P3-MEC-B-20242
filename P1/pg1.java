7529b9d95fea22a2ba5dd5a731b6df5344b84f38
Pabl11to <ov.pablo8@gmail.com>
2024-08-27 14:37:30 -0500

    Update pg1.java

--- P1/pg1.java
+++ P1/pg1.java
@@ -1,15 +1,30 @@
 /*Crear una clase llamada fracion con atributos correspondientes a numerador y denominador, aplicar encapsula, y los metodos set y get necesario*/
- 
-public class proyecto{
-  public class fraccion {
-    public int num;
-    public int denm;
-    
-    public set. 
-    
-  
-    
-    
+public class Fraccion {
+    // Atributos privados
+    private int numerador;
+    private int denominador;
+
+    // Constructor
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    // Métodos get
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    // Métodos set
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
 }
-   
- }
